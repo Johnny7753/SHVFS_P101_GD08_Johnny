c725589646a2f7fbd010a2ae5a4bc37857a52fcb
Johnny7753 <115707854+Johnny7753@users.noreply.github.com>
2022-11-10 21:31:31 +0800

    1

--- Quizzes/SHVFS_P101_GD08_Q2022-11-10_Johnny/SHVFS_P101_GD08_Q2022-11-10_Johnny/Program.cs
+++ Quizzes/SHVFS_P101_GD08_Q2022-11-10_Johnny/SHVFS_P101_GD08_Q2022-11-10_Johnny/Program.cs
@@ -8,15 +8,17 @@
             string secretword;
             string guessword;
             int i;
+            string[] classname = {"Johnny","Cloud","April","Oprah","Naomi","latte zia","Hardy","Yuki","Ar1es","Jerry","Naven","Mate","Bandy","Daderol" };
             Console.WriteLine($"Welcome! Please enter your name!");
             name=Console.ReadLine();
             Console.Clear();
             Random random = new Random();
-            int secret = random.Next(97, 123);
-            secretword=Convert.ToChar(secret).ToString();
+            int secret = random.Next(0,13);
+            secretword = classname[secret];
+            Console.WriteLine(secretword);
             for (i = 1; ;i++)
             {
-                Console.WriteLine($"Welcome {name}! Let's guess what the secret word is!\nPlease enter a word!");
+                Console.WriteLine($"Welcome {name}! Let's guess what the secret name is!\nPlease enter name of Johnny,Cloud,April,Oprah,Naomi,latte zia,Hardy,Yuki,Ar1es,Jerry,Naven,Mate,Bandy,Daderol!");
                 guessword = Console.ReadLine();
                 if (guessword==secretword) break;
                 if (guessword != secretword)
