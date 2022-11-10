c8438f83fa6f544747cb872d9f3c9b9b36750585
Johnny7753 <115707854+Johnny7753@users.noreply.github.com>
2022-11-10 20:28:06 +0800

    a

---
+++ Quizzes/SHVFS_P101_GD08_Q2022-11-10_Johnny/SHVFS_P101_GD08_Q2022-11-10_Johnny/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace SHVFS_P101_GD08_Q2_Johnny
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            string name;
+            string secretword;
+            string guessword;
+            Console.WriteLine($"Welcome! Please enter your name!");
+            name=Console.ReadLine();
+            Console.Clear();
+            Random random = new Random();
+            int secret = random.Next(97, 123);
+            secretword=Convert.ToString(secret);
+            for (int i = 0; ;i++)
+            {
+                Console.WriteLine($"Welcome {name}! Let's guess what the secret word is!\nPlease enter a word!");
+                guessword = Console.ReadLine();
+                if (guessword==secretword) break;
+                if (guessword != secretword)
+                {
+                    Console.WriteLine("You,are worng! Please enter another word!");
+                    Console.ReadLine();
+                }
+                Console.Clear();
+            }
+            Console.WriteLine($"You are right!");
+        }
+    }
+}
