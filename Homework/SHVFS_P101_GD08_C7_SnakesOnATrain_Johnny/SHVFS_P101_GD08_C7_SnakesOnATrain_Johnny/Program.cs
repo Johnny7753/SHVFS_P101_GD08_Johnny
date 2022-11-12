76d45a9e81505ba201f3e7b5dfab1711dc314c39
Johnny7753 <115707854+Johnny7753@users.noreply.github.com>
2022-11-12 17:51:08 +0800

    1

---
+++ Homework/SHVFS_P101_GD08_C7_SnakesOnATrain_Johnny/SHVFS_P101_GD08_C7_SnakesOnATrain_Johnny/Program.cs
@@ -0,0 +1,113 @@
+﻿using System.Numerics;
+
+namespace SHVFS_P101_GD08_C7_SnakesOnATrain
+{
+    public class Program
+    {
+        private static player player1 = new player()
+        {
+            Direction = position.Right,
+            InputDirections = new Dictionary<ConsoleKey, position>()
+            {
+                {
+                    ConsoleKey.W, position.UP
+                },
+                {
+                    ConsoleKey.S, position.Down
+                },
+                {
+                    ConsoleKey.A, position.Left
+                },
+                {
+                    ConsoleKey.D, position.Right
+                },
+            }
+        };
+        private static player player2 = new player()
+        {
+            Direction = position.Right,
+            InputDirections = new Dictionary<ConsoleKey, position>()
+            {
+                {
+                    ConsoleKey.UpArrow, position.UP
+                },
+                {
+                    ConsoleKey.DownArrow, position.Down
+                },
+                {
+                    ConsoleKey.LeftArrow, position.Left
+                },
+                {
+                    ConsoleKey.RightArrow, position.Right
+                },
+            }
+        };
+        public static void Main(string[] args)
+        {
+            var title = "Snakes On A Train";
+            Console.CursorLeft = Console.BufferWidth / 2 - title.Length / 2;
+            Console.WriteLine(title);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            var player1control = "Player 1's controls:\n" +
+                               "\n" +
+                               "W - Up\n" +
+                               "A - Left\n" +
+                               "S - Down\n" +
+                               "D - Right";
+            Console.WriteLine(player1control);
+            var player2control = "Player 2's controls:";
+            var cursorLeft = Console.BufferWidth - player2control.Length;
+            Console.CursorTop = 1;
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.CursorLeft=cursorLeft;
+            Console.WriteLine("Player 2's controls:\n");
+            Console.CursorLeft = cursorLeft;
+            Console.WriteLine("Up Arrow - Up");
+            Console.CursorLeft = cursorLeft;
+            Console.WriteLine("Left Arrow - Left");
+            Console.CursorLeft = cursorLeft;
+            Console.WriteLine("Down Arrow - Down");
+            Console.CursorLeft = cursorLeft;
+            Console.WriteLine("Right Arrow - Right");
+            Console.ReadKey();
+            Console.Clear();
+            SetupPlayArea();
+
+        }
+        public struct position
+        {
+            public int x;
+            public int y;
+            public position(int X,int Y)
+            {
+                x = X;
+                y = Y;
+            }
+            public static readonly position UP=new position(0,1);
+            public static readonly position Down = new position(0, -1);
+            public static readonly position Left = new position(-1, 0);
+            public static readonly position Right = new position(1, 0);
+        }
+        public class player
+        {
+            public position Position;
+            public position Direction;
+            public Dictionary<ConsoleKey, position> InputDirections;
+        }
+        private static void SetupPlayArea()
+        {
+            Console.WindowHeight = 30;
+            Console.BufferHeight = 30;
+            Console.WindowWidth = 100;
+            Console.BufferWidth = 100;
+            usedGridPositions = new bool[Console.WindowWidth, Console.WindowHeight];
+            player1.Direction = position.Right;
+            player1.Position.x = 0;
+            player1.Position.y = Console.WindowHeight/2;
+            player2.Direction = position.Left;
+            player2.Position.x = Console.WindowWidth-1;
+            player2.Position.y = Console.WindowHeight / 2;
+
+        }
+    }
+}
---
+++ Quizzes/SHVFS_P101_GD08_Q2022-11-11_JOHNNY/SHVFS_P101_GD08_Q2022-11-11_JOHNNY/Program.cs
@@ -0,0 +1,66 @@
+﻿
+using System.Reflection.Metadata.Ecma335;
+
+class Quizz
+{
+    public struct Position
+    {
+        public int x;
+        public int y;
+        public int z;
+        public Position(int X, int Y, int Z)
+        {
+            x = X;
+            y = Y;
+            z = Z;
+        }
+    }
+
+
+    public class GameObject
+    {
+        public string Name;
+        public Position pos;
+        public GameObject(string name, Position position)
+        {
+            Name = name;
+            pos = position;
+        }
+        public bool compare(GameObject game)
+        {
+            if (pos.x == game.pos.x && pos.y == game.pos.y && pos.z == game.pos.z)
+            {
+                return true;
+            }
+            else 
+            {
+                return false;
+            }
+        }
+        
+    }
+    public static void Main(string[] args)
+    {
+        var gameArray= new GameObject[5];
+        var game1 = new GameObject("1", new Position(0, 0, 0));
+        var game2 = new GameObject("2", new Position(1, 1, 1));
+        var game3 = new GameObject("3", new Position(2, 2, 2));
+        var game4 = new GameObject("4", new Position(2, 2, 2));
+        var game5 = new GameObject("5", new Position(1, 1, 1));
+        gameArray[0] = game1;
+        gameArray[1] = game2;
+        gameArray[2] = game3;
+        gameArray[3] = game4;
+        gameArray[4] = game5;
+        for(int i = 0; i < 4; i++)
+        {
+            for(int j = i+1; j <5; j++)
+            {
+                if (gameArray[i].compare(gameArray[j]))
+                {
+                    Console.WriteLine($"{gameArray[i].Name} is same as {gameArray[j].Name}");
+                }
+            }
+        }
+    }
+}
